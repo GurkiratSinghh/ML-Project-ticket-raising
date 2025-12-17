894d7132a28c69c767026f26c660602052632b46
Gurkirat Singh <gurkiratsinghh0001@gmail.com>
2025-12-17 23:07:01 +0530

    create a data_loader python file that can load and split data

---
+++ src/data_loader.py
@@ -0,0 +1,20 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+
+def load_and_split_data(csv_path, test_size=0.3, random_state=42):
+    df = pd.read_csv(csv_path)
+    df = df.drop(columns=["Unnamed: 0"], errors="ignore")
+
+    X = df["User Query"]
+    y_intent = df["User Intent"]
+    y_team = df["Assigned Team"]
+
+    X_train, X_test, y_intent_train, y_intent_test = train_test_split(
+        X, y_intent, test_size=test_size, random_state=random_state
+    )
+
+    # Align team labels with same split
+    y_team_train = y_team.loc[y_intent_train.index]
+    y_team_test = y_team.loc[y_intent_test.index]
+
+    return X_train, X_test, y_intent_train, y_intent_test, y_team_train, y_team_test
