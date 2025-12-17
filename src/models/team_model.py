59d471d6782eb38c4da66dd780e7ad0cef70940b
Gurkirat Singh <gurkiratsinghh0001@gmail.com>
2025-12-17 23:12:36 +0530

    creating model files with embeddings

---
+++ src/models/team_model.py
@@ -0,0 +1,6 @@
+from sklearn.linear_model import LogisticRegression
+
+def train_team_model(X_train_vec, y_train):
+    model = LogisticRegression(max_iter=1000, random_state=42)
+    model.fit(X_train_vec, y_train)
+    return model
